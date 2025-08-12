6b427110bfee9508cba523e35c1e188f740b804e
WooJinHong <orolzleim@gmail.com>
2025-08-12 21:43:59 +0900

    feat/초기 세팅

---
+++ src/main/java/com/post/kr/flowagent/FlowAgentApplication.java
@@ -0,0 +1,13 @@
+package com.post.kr.flowagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/post/kr/flowagent/FlowAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.kr.flowagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
