87d759eefbbfeb1680bd18cdcd0a05912ddb7d6a
WooJinHong <orolzleim@gmail.com>
2025-08-13 17:09:13 +0900

    Poetry 가상환경 설정 및 폴더 구조 변경

---
+++ SpringBoot/src/test/java/com/auto/kr/springboot/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.kr.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
